3f59ddb0e7a6a3e3862ec48df4b1f36903ca0799
artetreault16 <38506889+artetreault16@users.noreply.github.com>
2018-04-22 23:02:28 -0500

    Add files via upload

--- app/src/main/java/net/androidbootcamp/applesandoranges/GameScreen.java
+++ app/src/main/java/net/androidbootcamp/applesandoranges/GameScreen.java
@@ -2,13 +2,16 @@ package net.androidbootcamp.applesandoranges;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class GameScreen extends AppCompatActivity {
     int wallet = 0; //remember we have to pull this from storage
-    String playerName = ""; //remember we have to pull this from storage
     int appleTotal = 0, orangeTotal = 0, treeTotal = 0;    //total number of fruit - but is tree necessary?
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,24 @@ public class GameScreen extends AppCompatActivity {
         TextView txtPlayerName = (TextView) findViewById(R.id.txtPlayerName);
         TextView txtWallet = (TextView) findViewById(R.id.txtWallet);
 
+        //storage test stuff
+        try {
+            String FILENAME = "filePlayerName";
+            byte[] bytes = new byte[1024];
+
+            FileInputStream fos = openFileInput("filePlayerName");
+
+            fos.read(bytes);
+            fos.close();
+
+            String playerName = new String(bytes);
+            txtPlayerName.setText(playerName + "'s Farm");
+
+        } catch (IOException e) {
+            Log.e("ERROR", e.toString());
+        }
+
         //setting elements
-        txtPlayerName.setText(playerName + "'s Farm");
         txtWallet.setText("$" + wallet);
 
         //timer for apples
--- app/src/main/java/net/androidbootcamp/applesandoranges/StartScreen.java
+++ app/src/main/java/net/androidbootcamp/applesandoranges/StartScreen.java
@@ -1,19 +1,51 @@
 package net.androidbootcamp.applesandoranges;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class StartScreen extends AppCompatActivity {
-    String playerName = "";
+    String playerName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-        EditText txtPlayerName = (EditText) findViewById(R.id.txtPlayer);
+        final EditText txtPlayerName = (EditText) findViewById(R.id.txtPlayer);
+        Button btnGo = (Button) findViewById(R.id.btnGo);
 
-        playerName = txtPlayerName.getText().toString();
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        
+                try {
+                    String FILENAME = "filePlayerName";
+                    String playerName = txtPlayerName.getText().toString();
+
+                    FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+
+                    fos.write(playerName.getBytes());
+                    fos.close();
+
+                } catch (IOException e) {
+                    Log.e("ERROR", e.toString());
+                }
+
+                startActivity(new Intent(StartScreen.this, GameScreen.class));
+            }
+        });
+
     }
 }
