3d196cc71f095d34523bace2e6f0a9ed76d2a84b
artetreault16 <38506889+artetreault16@users.noreply.github.com>
2018-04-23 10:52:20 -0500

    Add files via upload

--- app/src/main/java/net/androidbootcamp/applesandoranges/GameScreen.java
+++ app/src/main/java/net/androidbootcamp/applesandoranges/GameScreen.java
@@ -1,5 +1,7 @@
 package net.androidbootcamp.applesandoranges;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,10 +9,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameScreen extends AppCompatActivity {
-    int wallet = 0; //remember we have to pull this from storage
+    int wallet; //remember we have to pull this from storage
     int appleTotal = 0, orangeTotal = 0, treeTotal = 0;    //total number of fruit - but is tree necessary?
 
     @Override
@@ -22,25 +27,35 @@ public class GameScreen extends AppCompatActivity {
         TextView txtPlayerName = (TextView) findViewById(R.id.txtPlayerName);
         TextView txtWallet = (TextView) findViewById(R.id.txtWallet);
 
-        //storage test stuff
+        //player name file
         try {
             String FILENAME = "filePlayerName";
             byte[] bytes = new byte[1024];
-
             FileInputStream fos = openFileInput("filePlayerName");
-
             fos.read(bytes);
             fos.close();
-
             String playerName = new String(bytes);
             txtPlayerName.setText(playerName + "'s Farm");
+        } catch (IOException e) {
+            Log.e("ERROR", e.toString());
+        }
+
+        //wallet file
+        try {
+            String FILENAME = "fileWallet";
+            String wallet = txtPlayerName.getText().toString();
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(wallet.getBytes());
+            fos.close();
 
         } catch (IOException e) {
             Log.e("ERROR", e.toString());
         }
 
         //setting elements
-        txtWallet.setText("$" + wallet);
+        txtWallet.setText(appleTotal);
+        appleTimer();
+        txtWallet.setText(appleTotal);
 
         //timer for apples
 
@@ -48,5 +63,18 @@ public class GameScreen extends AppCompatActivity {
 
         //go to shop
 
+    }
+
+    int appleTimer(){
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                appleTotal = appleTotal + 1;
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task, 10000);
+        return appleTotal;
     }
 }
