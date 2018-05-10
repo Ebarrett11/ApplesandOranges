769603d9d7569ec90b0e90f0ac48129989474dd0
Ebarrett11 <38506899+Ebarrett11@users.noreply.github.com>
2018-05-10 13:26:41 -0500

    Add files via upload

--- app/src/main/java/net/androidbootcamp/applesandoranges/StartScreen.java
+++ app/src/main/java/net/androidbootcamp/applesandoranges/StartScreen.java
@@ -2,13 +2,18 @@ package net.androidbootcamp.applesandoranges;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Handler;
+
+import org.w3c.dom.Text;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,16 +24,33 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-        final EditText txtPlayerName = findViewById(R.id.txtPlayer);
-        Button btnGo = findViewById(R.id.btnGo);
+        final EditText txtPlayerName = (EditText) findViewById(R.id.txtPlayer);
+        Button btnGo = (Button) findViewById(R.id.btnGo);
+        final TextView txtFarm = findViewById(R.id.txtFarm);
 
-        //AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
+
+
+        final Handler farmHandler = new Handler();
+        final int farmDelay = 10; //milliseconds
+
+        farmHandler.postDelayed(new Runnable(){
+            public void run(){
+
+                String name = txtPlayerName.getText().toString();
+                txtFarm.setText(name + "'s farm");
+
+                farmHandler.postDelayed(this, farmDelay);
+            }
+        }, farmDelay);
+
 
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 try {
+
                     String FILENAME = "filePlayerName";
                     String playerName = txtPlayerName.getText().toString();
 
